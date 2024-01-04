223b768b6cffe81601d442ff9459fe2bb08a706c
david-gimeno <dagigo1@dsic.upv.es>
2024-01-04 11:38:17 +0100

    (WIP) Prepare environment to run the toolkit

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-# -- installing Git LFS
-conda install conda-forge::git-lfs -y
-git lfs install
+set -e
 
-# -- installing iBug Face Detector
+# -- installing Git LFS
+# conda install conda-forge::git-lfs -y
+# git lfs install
+
+# -- installing submodules
+# git submodule init
+# git submodule update
+
+# -- -- installing the iBug face detection repo
+
+# cd ./modules/face_detection/ibug_facedetector/
+
+# git lfs pull
+# pip install -e .
+
+# cd ../../..
+
+# -- -- installing the iBug face alignment repo
+
+# cd ./modules/face_alignment/ibug_facealigner/
+
+# pip install -e .
+
+# cd ../../..
+
+# -- installing the rest of needed packages
+
